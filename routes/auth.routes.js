88a174c7e2964f141c77bce57072f364b58d58cc
Coops023 <bjorkc27@gmail.com>
2021-09-07 16:02:01 +0200

    registering user lesson

---
+++ routes/auth.routes.js
@@ -0,0 +1,39 @@
+const { Router } = require('express');
+const router = new Router();
+const User = require('../models/User.model');
+
+//get route => to display singup form
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+})
+
+
+router.get('/userProfile', (req, res) => {
+  res.render('users/user-profile')
+  //user.findbyid(id)
+})
+
+//post route to process the data
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+router.post("/signup", (req, res) => {
+  const { username, email, password } = req.body
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({ username, email, passwordHash: hashedPassword })
+        .then(userFromDB => {
+          res.redirect('/userProfile')
+        })
+        .catch(error => console.log('user was not created'))
+    })
+    .catch(error => console.log(error))
+
+})
+
+
+
+module.exports = router;
